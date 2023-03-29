6228364a4de1a6d9b597c4b616944bc437217b76
Keimah507 <githinjibrian5@gmail.com>
2023-03-30 00:15:04 +0300

    Re-route files

--- src/routes/index.js
+++ src/routes/index.js
@@ -41,6 +41,86 @@ router.get('/login', (req, res) => {
   res.sendFile(__dirname + '/src/login.html');
 });
 
+router.get('/marketplace', (req, res) => {
+  res.sendFile(__dirname + '/src/index-2.html');
+});
+
+router.get('/collection', (req, res) => {
+  res.sendFile(__dirname + '/src/collection.html');
+});
+
+router.get('/activity', (req, res) => {
+  res.sendFile(__dirname + '/src/activity.html');
+});
+
+router.get('/item-details', (req, res) => {
+  res.sendFile(__dirname + '/src/item-details.html');
+});
+
+router.get('/post-details', (req, res) => {
+  res.sendFile(__dirname + '/src/post-details.html');
+});
+
+router.get('/newsletter', (req, res) => {
+  res.sendFile(__dirname + '/src/newsletter.html');
+});
+
+router.get('/creatore', (req, res) => {
+  res.sendFile(__dirname + '/src/creatore.html');
+});
+
+router.get('/privacy', (req, res) => {
+  res.sendFile(__dirname + '/src/privacy.html');
+});
+
+router.get('/connect-wallet', (req, res) => {
+  res.sendFile(__dirname + '/src/wallet.html');
+});
+
+router.get('/privacy', (req, res) => {
+  res.sendFile(__dirname + '/src/privacy.html');
+});
+
+router.get('/faq', (req, res) => {
+  res.sendFile(__dirname + '/src/faq.html');
+});
+
+router.get('/chat', (req, res) => {
+  res.sendFile(__dirname + '/src/chat.html');
+});
+
+router.get('/article', (req, res) => {
+  res.sendFile(__dirname + '/src/artical.html');
+});
+
+router.get('/ranking', (req, res) => {
+  res.sendFile(__dirname + '/src/ranking.html');
+});
+
+router.get('/404', (req, res) => {
+  res.sendFile(__dirname + '/src/404.html');
+});
+
+router.get('/profile', (req, res) => {
+  res.sendFile(__dirname + '/src/profile.html');
+});
+
+router.get('/edit-profile', (req, res) => {
+  res.sendFile(__dirname + '/src/edit-profile.html');
+});
+
+router.get('/blog', (req, res) => {
+  res.sendFile(__dirname + '/src/blog.html');
+});
+
+router.get('/contact', (req, res) => {
+  res.sendFile(__dirname + '/src/contact.html');
+});
+
+router.get('/submit-request', (req, res) => {
+  res.sendFile(__dirname + '/src/submit-request.html');
+});
+
 router.get('/collection', (req, res) => {
   res.sendFile(__dirname + "/src/collection.html");
 });
