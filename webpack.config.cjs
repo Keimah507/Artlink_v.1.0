e37143d96bfcbb8a9cb2d329e03f151d33e21f27
Keimah507 <githinjibrian5@gmail.com>
2023-03-31 10:56:39 +0300

    Add module bundler

--- webpack.config.cjs
+++ webpack.config.cjs
@@ -2,7 +2,7 @@ const path = require('path');
 
 module.exports = {
   // The entry point file described above
-  entry: './index.js',
+  entry: './server.js',
   // The location of the build folder described above
   output: {
     path: path.resolve(__dirname, 'dist'),
