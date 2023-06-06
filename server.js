181f4a316c659e709db58a91022a49e984c3fac4
Keimah507 <githinjibrian5@gmail.com>
2023-06-07 02:12:34 +0300

    Add environment variable for port

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ app.use((req, res, next) => {
     return;
   });
 
-app.listen(5000, () => {
+app.listen(process.env.PORT || 5000, () => {
     console.log('Server listening on port 5000');
 });
 
